6c6963abc8d007c31d7ab3bddcca4ff8906f8a63
Joseungwan <wh7923@nhnent.com>
2016-10-04 17:04:55 +0900

    굿굿1등이다

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 	int number1, number2, result;
 	char operator;
 
-	printf("Please enter a expression: ");
+	printf("재미있는 충돌!! ");
 	scanf("%d %c %d", &number1, &operator, &number2);
 
 	switch (operator)
@@ -37,7 +37,7 @@ int main(int argc, char* argv[])
 int add(int number1, int number2)
 {
 	int result;
-	
+
 	result=number1+number2;
 	return result;
 }
